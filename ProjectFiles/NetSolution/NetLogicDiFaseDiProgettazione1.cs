111a231d4a1d4a140df7b676730f3fa746c8e73d
Stefano Spotti <stefano.spotti@maicopresse.com>
2023-05-11 14:14:27 +0200

    CommitProvaStefano

---
+++ ProjectFiles/NetSolution/NetLogicDiFaseDiProgettazione1.cs
@@ -0,0 +1,28 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.NetLogic;
+using FTOptix.CoreBase;
+using FTOptix.UI;
+using FTOptix.NativeUI;
+using FTOptix.Recipe;
+using FTOptix.EventLogger;
+using FTOptix.DataLogger;
+using FTOptix.SQLiteStore;
+using FTOptix.Store;
+using FTOptix.S7TiaProfinet;
+using FTOptix.Alarm;
+using FTOptix.CommunicationDriver;
+using FTOptix.Core;
+#endregion
+
+public class NetLogicDiFaseDiProgettazione1 : BaseNetLogic
+{
+    [ExportMethod]
+    public void Method1()
+    {
+        // Insert code to be executed by the method
+    }
+}
---
+++ ProjectFiles/NetSolution/NetLogicDiRuntime1.cs
@@ -0,0 +1,33 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.NetLogic;
+using FTOptix.CoreBase;
+using FTOptix.UI;
+using FTOptix.NativeUI;
+using FTOptix.Recipe;
+using FTOptix.EventLogger;
+using FTOptix.DataLogger;
+using FTOptix.SQLiteStore;
+using FTOptix.Store;
+using FTOptix.S7TiaProfinet;
+using FTOptix.Alarm;
+using FTOptix.CommunicationDriver;
+using FTOptix.Core;
+using FTOptix.ODBCStore;
+#endregion
+
+public class NetLogicDiRuntime1 : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
